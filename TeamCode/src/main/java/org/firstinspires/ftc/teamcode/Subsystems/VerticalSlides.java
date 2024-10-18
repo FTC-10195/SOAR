6a732817afe33ccdbb6105de3cfab12eddcf1610
DoctorKrill08 <doctorkrill08@gmail.com>
2024-10-18 17:41:14 -0400

    Set up each function

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/VerticalSlides.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/VerticalSlides.java
@@ -2,7 +2,7 @@ package org.firstinspires.ftc.teamcode.Subsystems;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-public class Arm {
+public class VerticalSlides {
     //Set up your variables ex: DcMotor frontLeftMotor;
     public  void initiate(HardwareMap hardwareMap){
         //Declare your variables ex: frontLeftMotor = hardwareMap.dcMotor.get("motor1");
