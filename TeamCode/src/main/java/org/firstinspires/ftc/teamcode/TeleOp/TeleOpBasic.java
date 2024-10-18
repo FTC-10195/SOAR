e850412840d4fec9d0f9bffa1eb175d45620ab4f
DoctorKrill08 <doctorkrill08@gmail.com>
2024-10-18 17:57:03 -0400

    renamed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOpBasic.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOpBasic.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.teamcode.Subsystems.Arm;
+import org.firstinspires.ftc.teamcode.Subsystems.Extendo;
 import org.firstinspires.ftc.teamcode.Subsystems.DriveTrain;
 import org.firstinspires.ftc.teamcode.Subsystems.Intake;
 import org.firstinspires.ftc.teamcode.Subsystems.VerticalSlides;
@@ -13,14 +13,14 @@ import org.firstinspires.ftc.teamcode.Subsystems.Wrist;
 public class TeleOpBasic extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
-        Arm arm = new Arm();
+        Extendo extendo = new Extendo();
         DriveTrain driveTrain = new DriveTrain();
         Intake intake = new Intake();
         VerticalSlides verticalSlides = new VerticalSlides();
         Wrist wrist = new Wrist();
         waitForStart();
         if (isStopRequested()) return;
-        arm.initiate(hardwareMap);
+        extendo.initiate(hardwareMap);
         driveTrain.initiate(hardwareMap);
         intake.initiate(hardwareMap);
         verticalSlides.initiate(hardwareMap);
@@ -29,7 +29,7 @@ public class TeleOpBasic extends LinearOpMode {
             double y = -gamepad1.left_stick_y; // Remember, Y stick is reversed!
             double x = gamepad1.left_stick_x * 1.1;
             double rx = gamepad1.right_stick_x;
-            arm.run();
+            extendo.run();
             driveTrain.run();
             intake.run();
             verticalSlides.run();
