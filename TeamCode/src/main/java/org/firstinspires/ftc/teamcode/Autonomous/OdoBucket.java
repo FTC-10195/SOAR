dc3d20b1c5605a5974a4f985bc339636e90de02a
DoctorKrill08 <doctorkrill08@gmail.com>
2025-01-26 00:50:34 -0500

    CHANGED

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/OdoBucket.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/OdoBucket.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
 
 
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.ParallelAction;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.SequentialAction;
@@ -10,6 +11,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.MecanumDrive;
+import org.firstinspires.ftc.teamcode.PinpointDrive;
 import org.firstinspires.ftc.teamcode.Subsystems.Arm;
 import org.firstinspires.ftc.teamcode.Subsystems.VerticalSlides;
 
@@ -19,13 +21,19 @@ import com.acmerobotics.roadrunner.ftc.Actions;
 public class OdoBucket extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
-        Pose2d beginPose = new Pose2d(-24, -63.75, Math.toRadians(180));
-        MecanumDrive mecanumDrive = new MecanumDrive(hardwareMap, beginPose);
-        Arm arm = new Arm();
+        double robotLength = 17.1;
+        double robotWidth = 16.38;
+        double tileSize = 24;
+        double scoreOffset = 3;
+        double barrierOffset = 0;
+        Pose2d beginPose = new Pose2d(0, 0 + barrierOffset, Math.toRadians(0));
+        PinpointDrive drive = new PinpointDrive(hardwareMap, beginPose);        Arm arm = new Arm();
         VerticalSlides verticalSlides = new VerticalSlides();
         verticalSlides.initiate(hardwareMap);
         arm.initiate(hardwareMap);
         waitForStart();
+        Vector2d preScorePos = new Vector2d(0 , (tileSize - robotWidth));
+        Vector2d scorePos = new Vector2d(0 - scoreOffset, (tileSize - robotWidth) -scoreOffset);
         if (isStopRequested()) return;
         while (opModeIsActive()) {
             Actions.runBlocking(
@@ -34,21 +42,24 @@ public class OdoBucket extends LinearOpMode {
                             new SequentialAction(
                                     arm.intakeAction(Arm.Intake.CLOSE),
                                     new ParallelAction(
-                                            mecanumDrive.actionBuilder(beginPose)
-                                                    .strafeToLinearHeading(new Vector2d(-48, -48), Math.toRadians(225))
+                                            drive.actionBuilder(beginPose)
+                                                    .strafeToLinearHeading(preScorePos, Math.toRadians(45))
                                                     .build(),
                                             arm.bucketAction(),
-                                            verticalSlides.slideAction(VerticalSlides.SlidePositions.BUCKET)
+                                            arm.shoulderAction(Arm.Shoulder.BACKWARDS),
+                                            arm.wristAction(Arm.Wrist.FORWARD)
+                                        //    verticalSlides.slideAction(VerticalSlides.SlidePositions.BUCKET)
                                     ),
-                                    mecanumDrive.actionBuilder(beginPose)
-                                            .strafeToLinearHeading(new Vector2d(-60, -60), Math.toRadians(225))
-                                            .build(),
-                                    arm.intakeAction(Arm.Intake.OPEN),
                                     new SleepAction(.5),
-                                    mecanumDrive.actionBuilder(beginPose)
-                                            .strafeToLinearHeading(new Vector2d(-48, -48), Math.toRadians(225))
+                                    drive.actionBuilder(beginPose)
+                                            .strafeToLinearHeading(scorePos, Math.toRadians(45))
                                             .build(),
-                                    verticalSlides.slideAction(VerticalSlides.SlidePositions.DOWN)
+                                    new SleepAction(2),
+                                    arm.intakeAction(Arm.Intake.OPEN),
+                                    arm.shoulderAction(Arm.Shoulder.BACKWARDS),
+                                    new SleepAction(.5),
+                                   //  verticalSlides.slideAction(VerticalSlides.SlidePositions.DOWN),
+                                    arm.shoulderAction(Arm.Shoulder.FORWARDS)
                             ),
                             verticalSlides.updateAction()
                     )
